c3588851aaa1cdd49d4b906f6a263aa78279563c
uffo <uffo686@gmail.com>
2021-05-13 08:48:09 +0300

    now translating...

---
+++ src/DeepL.js
@@ -0,0 +1,52 @@
+const axios = require('axios')
+
+class DeepL {
+    constructor( auth_key, target_lang, free = true ){
+        this.auth_key = auth_key
+        this.target_lang = target_lang
+        this.api = free ? 'https://api-free.deepl.com/v2/' : 'https://api.deepl.com/v2/'
+    }
+
+    translate(text, source_lang = 'en'){
+        return new Promise((resolve) => {
+            var config = {
+                method: 'post',
+                url: `${this.api}translate?auth_key=${this.auth_key}&target_lang=${this.target_lang}&source_lang=${source_lang}&tag_handling=xml&split_sentences=nonewlines&ignore_tags=NoTrans&text=${encodeURIComponent(text)}`,
+                headers: {
+                    "Content-Type": 'application/x-www-form-urlencoded'
+                }
+            };
+
+            // console.log(config)
+
+            axios(config)
+            .then(function (response) {
+                // console.table(response.data.text)
+                resolve(response.data)
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        })
+    }
+    usage(){
+        var config = {
+            method: 'post',
+            url: `${this.api}usage?auth_key=${this.auth_key}`,
+            headers: { }
+          };
+
+          return axios(config)
+          .then(function (response) {
+            console.log(JSON.stringify(response.data));
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+    }
+}
+
+module.exports = {
+    DeepL
+}
--- src/index.js
+++ src/index.js
@@ -1,27 +1,125 @@
 const inquirer = require('inquirer')
 const gettextParser = require("gettext-parser")
-const { writeFileSync } = require('fs')
+const { readFileSync, writeFileSync, existsSync } = require('fs')
+const { program } = require('commander')
+const { DeepL } = require('./deepl')
+const ConsoleProgressBar = require('console-progress-bar')
+const noop = () => {}
 
+function welcome() {
+    console.log('Translate your app/site or service .po files? Read this https://github.com/uffou/deepl-po-translate for more info' );
+    console.log('------------------');
+
+};
+
+program
+  .option('-d, --debug', 'output extra debugging')
+  .option('-a, --api <api>', 'free or paid api')
+  .option('-k, --auth-key <auth_key>', 'auth_key')
+  .option('-l, --po-file-path <target_lang>', '.po File Path')
+  .option('-l, --target-lang <target_lang>', 'Target language')
+
+program.parse(process.argv);
+
+const options = program.opts();
+let answers
+if (options.debug) console.log(options);
+console.log('pizza details:');
+if (options.api) console.log(options.api);
+if (options.authKey) console.log(options.authKey);
+if (options.targetLang) console.log(options.targetLang);
+if (options.poFilePath) console.log(options.poFilePath);
+const debug = options.debug ? console.log : noop
+
+welcome()
+if(options)
 inquirer.prompt([
     { type: 'list', message: "Free or paid api?", name:'api',choices:[
         "DeepL Free API (api-free.deepl.com)",
         "DeepL Paid API (api.deepl.com)"
     ]},
-    {
-        type:'input',
-        message: 'auth_key',
-        name: 'auth_key',
-    },
-    {
-        type:'input',
-        message: 'Target language',
-        name: 'target_lang',
-    },
+    // {
+    //     type:'input',
+    //     message: 'auth_key',
+    //     name: 'authKey',
+    // },
+    // {
+    //     type:'input',
+    //     message: 'Source File Path',
+    //     name: 'poFilePath',
+    // },
+    // {
+    //     type:'input',
+    //     message: 'Target language',
+    //     name: 'targetLang',
+    // },
 
 ]).then(answers => {
+    answers = answers
+
     console.log(answers)
-    
+    parseAndTranslate()
+})
+
+function escapeString(string){
+    return string.replace(/{(.*)}/gm, '<NoTrans>$&</NoTrans>')
+}
+function unescapeString(string){
+    return string.replace(/<NoTrans>/gm, '').replace(/<\/NoTrans>/gm, '')
+
+}
+async function parseAndTranslate(){
     const cwd = process.cwd()
     console.log(cwd)
-    writeFileSync(cwd + '/' + answers.target_lang + '.po', 'test')
-})
+
+    const poFilePath = options.poFilePath || (answers && answers.poFilePath)
+    if(!existsSync(poFilePath)) throw 'PO file not found!'
+
+    const deepl = new DeepL(options.authKey, options.targetLang)
+
+    var input = readFileSync(poFilePath)
+    var po = gettextParser.po.parse(input)
+
+    const transObject = po.translations['']
+    let keysWithoutTranslation = []
+    Object.keys(transObject).forEach(key => {
+        //TODO: handle multiline texts
+        if(transObject[key].msgstr[0] === '') {
+            keysWithoutTranslation.push(key)
+            debug('\x1b[33m\x1b[0m',transObject[key].msgstr)
+        }
+    })
+
+    if(keysWithoutTranslation.length === 0) return console.log('All strings translated')
+    console.log('Translating:', keysWithoutTranslation.length, ' strings')
+
+    function saveFile(){
+        const output = gettextParser.po.compile(po)
+        writeFileSync(poFilePath, output)
+    }
+
+    // keysWithoutTranslation = keysWithoutTranslation.slice(0,10) // TESTING
+    const bar = new ConsoleProgressBar({ maxValue: keysWithoutTranslation.length })
+
+    index = 0
+    for(const key of keysWithoutTranslation){
+        index += 1
+        const sourceString = escapeString(transObject[key].msgid)
+        const data = await deepl.translate(sourceString).catch(error => {
+            console.error(error)
+            deepl.usage()
+        })
+        // console.log(data)
+        // TODO HANDLE ARRAYS
+        transObject[key].msgstr[0] = unescapeString(data.translations[0].text)
+        debug('\x1b[32m%s\x1b[0m',transObject[key].msgstr[0])
+        bar.addValue(1)
+        if(index % 20 === 0) {
+            saveFile() // saving each 20 api calls just in case
+        }
+    }
+
+    saveFile()
+    console.log('\nSaved...')
+    deepl.usage()
+}
